6b3251712066865c067e31bff99434114af46d11
ValerieMbaka <valeriembaka0@gmail.com>
2023-05-13 10:36:00 +0300

    first commit

---
+++ app/src/main/java/com/example/myworkoutapp/DiscoverActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myworkoutapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiscoverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discover)
+    }
+}
