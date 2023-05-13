8f62fbcb657fbbe90eb0d02ab0d0e9fad80e653e
ValerieMbaka <valeriembaka0@gmail.com>
2023-05-13 12:12:39 +0300

    First commit

--- app/src/main/java/com/example/myworkoutapp/SplashActivity.kt
+++ app/src/main/java/com/example/myworkoutapp/SplashActivity.kt
@@ -6,10 +6,10 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 
-class LoadActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_load)
+        setContentView(R.layout.activity_splash)
         val handler = Handler(Looper.getMainLooper())
         handler.postDelayed({
             val intent = Intent(this, SignUpActivity::class.java)
