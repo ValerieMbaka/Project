5eeaea6ca19e3b5c9936a5059128089a44c2a32d
ValerieMbaka <valeriembaka0@gmail.com>
2023-05-15 13:48:48 +0300

    Third commit

--- app/src/main/java/com/example/myworkoutapp/HistoryActivity.kt
+++ app/src/main/java/com/example/myworkoutapp/HistoryActivity.kt
@@ -1,9 +1,11 @@
 package com.example.myworkoutapp
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class HistoryActivity : AppCompatActivity() {
+    private lateinit var db:SQLiteDatabase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
---
+++ app/src/main/java/com/example/myworkoutapp/HistoryAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.myworkoutapp
+
+class HistoryAdapter {
+}
