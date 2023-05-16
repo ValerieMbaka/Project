17014703508ebc1f49b4173be28f8ba66d9c6a51
ValerieMbaka <valeriembaka0@gmail.com>
2023-05-16 13:35:27 +0300

    Seventh commit

--- app/src/main/java/com/example/myworkoutapp/SettingsActivity.kt
+++ app/src/main/java/com/example/myworkoutapp/SettingsActivity.kt
@@ -3,26 +3,26 @@ package com.example.myworkoutapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.cardview.widget.CardView
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
+import com.example.myworkoutapp.databinding.ActivitySettingsBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class SettingsActivity : AppCompatActivity() {
-    lateinit var cardProfile: CardView
-    lateinit var cardLogout: CardView
+    private lateinit var binding: ActivitySettingsBinding
+    private lateinit var user:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        cardProfile = findViewById(R.id.cardProfile)
-        cardProfile.setOnClickListener {
-            val profile = Intent(this, ProfileActivity::class.java)
-            startActivity(profile)
-        }
+        user = FirebaseAuth.getInstance()
 
-        cardLogout = findViewById(R.id.cardLogout)
-        cardLogout.setOnClickListener {
-            Firebase.auth.signOut()
+        binding.cardLogout.setOnClickListener {
+            user.signOut()
+            startActivity(
+                Intent(this, MainActivity::class.java)
+            )
+            finish()
         }
     }
 }
